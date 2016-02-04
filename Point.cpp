a410db9baac0540257371ba2a801434df004d03e
Gannon Faul <gannon.faul@ucdenver.edu>
2016-02-04 16:28:44 -0700

    Implemented area.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,19 +1,15 @@
 //
 // Created by Gannon Faul on 2/4/16.
 //
-
 #include <iostream>
 #include <cmath>
 
 #include "Point.h"
 
-
-
-
 using namespace std;
 
-// CONSTRUCTORS
-// ------------
+// **CONSTRUCTORS**
+
 // default constructor
 Point::Point(){
     __x = 0;
@@ -26,45 +22,28 @@ Point::Point(double x, double y, double z) {
     __x = x;
     __y = y;
     __z = z;
-
 }
 
-// METHODS
-// -------
+// **METHODS**
+
 // Mutators
-void Point::setX(double newX) {
-    __x = newX;
-}
-
-void Point::setY(double newY) {
-    __y = newY;
-}
-
-void Point::setZ(double newZ) {
-    __z = newZ
-}
+void Point::setX(double newX) { __x = newX; }
+void Point::setY(double newY) { __y = newY; }
+void Point::setZ(double newZ) { __z = newZ; }
 
 // Accessors
-double Point::getX() const {
-    return __x;
-}
-
-double Point::getY() const {
-    return __y;
-}
-
-double Point::getZ() const {
-    return __z;
-}
+double Point::getX() const { return __x; }
+double Point::getY() const { return __y; }
+double Point::getZ() const { return __z; }
 
 // Distance Calculator
 double Point::distanceTo(const Point &p) const {
 
     double distance;
 
-    double xDist = __x - p.__x;
-    double yDist = __y - p.__y;
-    double zDist = __z - p.__z;
+    double xDist = __x - p.getX();
+    double yDist = __y - p.getY();
+    double zDist = __z - p.getZ();
 
     distance = sqrt(pow(xDist, 2) + pow(yDist, 2) + pow(zDist, 2));
 
--- area.cpp
+++ area.cpp
@@ -3,12 +3,21 @@
 //
 
 #include <iostream>
-#include <iomanip>
 #include <cmath>
-#include <cassert>
 
 #include "Point.h"
 
 using namespace std;
 
+double computeArea(const Point &a, const Point &b, const Point &c){
+    // Find lengths of each side
+    double sideA = a.distanceTo(b);
+    double sideB = a.distanceTo(c);
+    double sideC = b.distanceTo(c);
+    // Find semiperimeter
+    double s = 0.5 * (sideA + sideB + sideC);
 
+    double area = sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+
+    return area;
+}
