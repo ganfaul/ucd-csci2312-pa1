91eda3c5e88c81f3d36c1cf19fd862f49d22d21a
Gannon Faul <gannon.faul@ucdenver.edu>
2016-02-04 16:33:03 -0700

    Added comments, all 56 test passed!

--- area.cpp
+++ area.cpp
@@ -9,14 +9,18 @@
 
 using namespace std;
 
+// Calculates and returns the area between three pointsAdd
 double computeArea(const Point &a, const Point &b, const Point &c){
+
     // Find lengths of each side
     double sideA = a.distanceTo(b);
     double sideB = a.distanceTo(c);
     double sideC = b.distanceTo(c);
+
     // Find semiperimeter
     double s = 0.5 * (sideA + sideB + sideC);
 
+    // Find area
     double area = sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
 
     return area;
